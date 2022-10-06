074c572c527420f22c0c10dc92fde741ffb78e7f
matafur <migueltafurr@gmail.com>
2022-10-06 17:47:39 -0500

    pagina-documentada

--- assets/js/registrarse.js
+++ assets/js/registrarse.js
@@ -1,13 +1,16 @@
+//declaracion de variables
 let name = document.getElementById("name")
 let phone = document.getElementById("phone")
 let email = document.getElementById("email")
 let password = document.getElementById("password")
 let signup = document.getElementById("btnSignup")
 let form = document.getElementById("form")
+//fin declaracion de variables
 
+//declaracion de fucniones
 signup.addEventListener("click", function () {
     form.classList.add('was-validated')
-    if (form.checkValidity()) {
+    if (form.checkValidity()) { //condicional if para validar si el usuario se puede registrar
         if (window.localStorage.getItem(email.value)) {
             console.error("Ya existe el usuario")
             showToast("El usuario ya se encuentra registrado")
@@ -16,15 +19,15 @@ signup.addEventListener("click", function () {
             form.classList.remove('was-validated')
             form.reset()
             showToast("¡Usuario registrado con exito!")
-        }
-    } else {
+        } //validacion donde se alojan los datos en el local storage
+    } else { // validacion en caso de que los datos esten incompletos
         console.error("Primero revise validaciones")
         showToast("Formulario incompleto")
-    }
+    } //fin delvalidacion de registro
     
 });
 
-function showToast(msg) {
+function showToast(msg) { //funcionshowtoast para las ventanas emergentes de notificacion
     let spanMsg = document.getElementById("msg")
     spanMsg.innerHTML = msg
     var toastElList = [].slice.call(document.querySelectorAll('.toast'))
